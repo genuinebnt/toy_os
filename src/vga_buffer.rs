b4e9cff5a2796692d7cc8f8ee073fbbfd65d19a1
genuinebasilnt <genuinebnt@gmail.com>
2025-02-03 08:40:33 +0530

    testing

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -42,8 +42,8 @@ struct ScreenChar {
     color_code: ColorCode,
 }
 
-const BUFFER_WIDTH: usize = 25;
-const BUFFER_HEIGHT: usize = 80;
+const BUFFER_WIDTH: usize = 80;
+const BUFFER_HEIGHT: usize = 25;
 
 #[repr(transparent)]
 struct Buffer {
@@ -140,4 +140,27 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "abcdefghijklmnopqrstuvwxyz0123456789";
+    println!("{}", s);
+
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,30 +4,22 @@
 #![test_runner(toy_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+
+use toy_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, Word!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     toy_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
